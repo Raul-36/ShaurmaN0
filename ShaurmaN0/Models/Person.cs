7cab0368864382a14a2e83d361a6b33a0b034657
Rau-36 <raul363636r36d@gmail.com>
2024-04-16 18:00:39 +0400

    Add models

---
+++ ShaurmaN0/Models/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShaurmaN0.Models;
+
+    public class Person{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Password { get; set; }
+    }
