05ffa86e200234c89d46c5d4e84fa692311c00b9
Rau-36 <raul363636r36d@gmail.com>
2024-06-10 13:13:37 +0400

    Add AddMinuItem

--- ShaurmaN0App/Controllers/MenuItemController.cs
+++ ShaurmaN0App/Controllers/MenuItemController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using ShaurmaN0App.Models;
@@ -5,16 +6,35 @@ namespace ShaurmaN0App.Controllers;
 
 
 [Route("/[controller]/[action]")]
-class MenuItemController : Controller
+public class MenuItemController : Controller
 {
-        [ActionName("Info")]
-        public async Task<IActionResult> GetMenuItemAsync() {
-        var MenuItemJson = await System.IO.File.ReadAllTextAsync("./Assets/MenuItem.json");
-        var MenuItem = JsonSerializer.Deserialize<MenuItem>(MenuItemJson);
-        return base.View(model: MenuItem);
-        }
-        public  IActionResult Index()
+        [ActionName("MenuItems")]
+        public async Task<IActionResult> GetMenuItemsAsync() {
+        var menuItemsJson = await System.IO.File.ReadAllTextAsync("./Assets/MenuItem.json");
+        var menuItems = JsonSerializer.Deserialize<IEnumerable<MenuItem>>(menuItemsJson, new JsonSerializerOptions
         {
-                return View();
+                PropertyNameCaseInsensitive = true
+        });
+        return base.View(model: menuItems);
         }
+
+        [ActionName("AddMenuItemApi")]
+        [HttpPost]
+        public async Task<IActionResult> AddMenuItemApi([FromForm] MenuItem menuItem) { 
+                var menuItemsJson = await System.IO.File.ReadAllTextAsync("./Assets/MenuItem.json");
+                var menuItems = JsonSerializer.Deserialize<IEnumerable<MenuItem>>(menuItemsJson, new JsonSerializerOptions
+                {
+                        PropertyNameCaseInsensitive = true
+                });
+
+                var newMenuItems = menuItems.Append(menuItem);
+                System.IO.File.WriteAllText("./Assets/MenuItem.json", JsonSerializer.Serialize(newMenuItems));
+                return base.Redirect("MenuItems");
+        }
+        [ActionName("AddMenuItem")]
+        [HttpGet]
+        public async Task<IActionResult> AddMenuItem() {
+                return base.View(model: new MenuItem {});
+        }
+
 }
