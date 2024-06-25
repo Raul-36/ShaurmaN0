886217f29d7dde71548a6a881a0ceffa09d840a3
Rau-36 <raul363636r36d@gmail.com>
2024-06-25 12:35:47 +0400

    Add EF

--- ShaurmaN0App/Program.cs
+++ ShaurmaN0App/Program.cs
@@ -1,4 +1,6 @@
-using ShaurmaN0App.Middleware;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using ShaurmaN0App.Data;
 using ShaurmaN0App.Repositories;
 using ShaurmaN0App.Repositories.Base;
 
@@ -7,7 +9,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<IMenusRepository, MenusSQLRepository>();
 builder.Services.AddTransient<IMenusCategoryRepository, MenusCategorySQLRepository>();
-
+builder.Services.AddDbContext<ShaurmaDbContext>(dbContextOptionsBuilder => {
+    var connectionString = builder.Configuration.GetConnectionString("SqlDb");
+    dbContextOptionsBuilder.UseSqlServer(connectionString);
+});
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
@@ -19,7 +24,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseMiddleware<LoggingMiddleware>();
+//app.UseMiddleware<LoggingMiddleware>();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
